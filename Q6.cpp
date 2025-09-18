7a80d18f2f2cee688e672b4d7a71fb95a55c3da0
YuvrajSingh7625 <ysingh8_be24@thapar.edu>
2025-09-18 14:46:58 +0530

    Add files via upload

---
+++ Q6.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cin >> r >> c;
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    cout << "Original Matrix:" << endl;
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << "Transpose Matrix:" << endl;
+    int transpose[c][r];
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            transpose[i][j] = a[j][i];
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    bool symmetric = true;
+    if (r == c) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (a[i][j] != transpose[i][j]) {
+                    symmetric = false;
+                    break;
+                }
+            }
+            if (!symmetric) break;
+        }
+        cout << "Symmetric: " << (symmetric ? "Yes" : "No") << endl;
+    } else {
+        cout << "Symmetric: No (Not a square matrix)" << endl;
+    }
+
+    int zeroCount = 0;
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            if (a[i][j] == 0)
+                zeroCount++;
+    if (zeroCount > (r * c) / 2)
+        cout << "Sparse: Yes" << endl;
+    else
+        cout << "Sparse: No" << endl;
+
+    return 0;
+}
